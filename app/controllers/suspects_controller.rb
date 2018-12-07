7a52640275b3ee7dabb780a31eda1784c6ec6cf7
= <siheonl@andrew.cmu.edu>
2018-12-07 14:38:25 -0500

    added suspect create method

--- app/controllers/suspects_controller.rb
+++ app/controllers/suspects_controller.rb
@@ -2,8 +2,22 @@ class SuspectsController < ApplicationController
     before_action :check_login
   
     def new
-      authorize! :new, @officer
+      authorize! :new, @suspect
       @suspect = Suspect.new
+    end
+    
+    def create
+      authorize! :new, @suspect
+      @suspect = Suspect.new(suspect_params)
+      if @suspect.save
+        flash[:notice] = "Successfully added #{@suspect.criminal.proper_name} on the investigation #{@suspect.investgation.title}."
+        # redirect can be show_investigation or criminal_investigation
+        redirect_to investigation_path(@suspect.investigation)
+      else
+        # error message 
+        flash[:notice] = "Failed to add #{@suspect.criminal.proper_name} on the investigation #{@suspect.investgation.title}."
+        render action: 'new'
+      end      
     end
 
     # only set dropped_on to current date
@@ -13,22 +27,13 @@ class SuspectsController < ApplicationController
         @suspect.dropped_on = Date.current.to_date
         @suspect.saves
         # redirect can be show_investigation or criminal_investigation
-
         redirect_to investigation_path(@suspect.investigation)
     end
-  
-  
-  
-  
-  
+
     private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_officer
-      @officer = Officer.find(params[:id])
-    end
     # Never trust parameters from the scary internet, only allow the white list through.
-    def officer_params
-      params.require(:officer).permit(:first_name, :last_name, :rank, :ssn, :active, :unit_id)
+    def suspect_params
+      params.require(:suspect).permit(:criminal_id, :investigation_id, :added_on, :dropped_on)
     end
   
   
